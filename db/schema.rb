dc261d2aad0a57ab392265253056de8270862b52
Omar Kazeb <kazeb.omar@gmail.com>
2018-08-27 18:43:49 +0100

    Generated models for order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_173114) do
+ActiveRecord::Schema.define(version: 2018_08_27_174315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,26 @@ ActiveRecord::Schema.define(version: 2018_08_27_173114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_designs_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "address"
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "size"
+    t.integer "quantity"
+    t.integer "unit_price"
+    t.bigint "design_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["design_id"], name: "index_products_on_design_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +59,7 @@ ActiveRecord::Schema.define(version: 2018_08_27_173114) do
   end
 
   add_foreign_key "designs", "users"
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "users"
+  add_foreign_key "products", "designs"
 end
