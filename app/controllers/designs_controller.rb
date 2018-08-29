d67f4435d0bae3059872d2847c876b64dc11b104
Yasmina Benchekroun <yasmina.benchekroun@gmail.com>
2018-08-29 16:11:27 +0100

    create new design v1

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -1,4 +1,5 @@
 class DesignsController < ApplicationController
+  before_action :set_design, only: [:show, :edit, :update, :destroy]
 
   def index
     @designs = Design.all
@@ -7,6 +8,41 @@ class DesignsController < ApplicationController
   def show
     @design = Design.find(params[:id])
     @product = Product.new
+  end
+
+  def new
+    @design = Design.new
+  end
+
+  def create
+    @design = Design.new(design_params)
+    if @design.save
+      @design.create
+      redirect_to design_path(@design)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    redirect_to designs_path
+  end
+
+  private
+  def design_params
+    params.require(:design).permit(:name, :theme, :photo)
+  end
+
+  def set_design
+    @design = Design.find(params[:id])
   end
 
 end
---
+++ app/javascript/components/dropzone.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+import * as Dropzone from "dropzone";
+
+function initDropZone() {
+    Dropzone.autoDiscover = false;
+
+    let myDropzone = new Dropzone("#new_design", {
+        url: $('#new_design').action,
+        maxFilesize: 3,
+        maxFiles: 1,
+        paramName: "design[photo]",
+        addRemoveLinks: true, // Don't show remove links on dropzone itself.
+        autoProcessQueue: false,
+        dictRemoveFile: "Remove design image",
+        sendingmultiple: function(data, xhr, formData) {
+            formData.append("design[name]", $("#design_name").val());
+            formData.append("design[theme]", $("#design_theme").val());
+        }
+    });
+
+    myDropzone.on("success", function(file, serverResponse) {
+        /* Maybe display some more file information on your page */
+        eval(serverResponse);
+    });
+
+    $('#new_design').submit(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        myDropzone.processQueue();
+    });
+
+    console.log("dropzone initialized");
+}
+
+export {initDropZone}
